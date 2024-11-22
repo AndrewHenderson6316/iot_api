57b61f5b251a81ce106dd08ad3d1cc054001ee4d
EtienneBoutet8561234 <113942723+EtienneBoutet8561234@users.noreply.github.com>
2024-11-22 18:22:53 -0500

    push1

--- app/controllers/data_entries_controller.rb
+++ app/controllers/data_entries_controller.rb
@@ -15,46 +15,46 @@ class DataEntriesController < ApplicationController
   # POST /groups
   def create
     # Permit nested parameters for data creation
-    data = params.require(:data_entry).permit(:value, data_type: {}, device: {}, time_of_sample: {}, sensor: {})
-  
+    #data = params.require(:data_entry).permit(:value, data_type: {}, device: {}, time_of_sample: {}, sensor: {})
+    data = data_entry_params
     ActiveRecord::Base.transaction do
       # Logging the start of the transaction
       Rails.logger.debug("Starting transaction...")
   
       # DataType
-      Rails.logger.debug("Received data_type: #{data[:data_type]}")
-      data_type = DataType.find_or_create_by!(typeName: data[:data_type][:typeName]) do |dt|
-        dt.scale = data[:data_type][:scale]
-        dt.description = data[:data_type][:description]
+      Rails.logger.debug("Received data_type: #{data[:data_type_attributes]}")
+      data_type = DataType.find_or_create_by!(typeName: data[:data_type_attributes][:typeName]) do |dt|
+        dt.scale = data[:data_type_attributes][:scale]
+        dt.description = data[:data_type_attributes][:description]
       end
       Rails.logger.debug("Created/Found data_type: #{data_type.inspect}")
   
       # Device
-      Rails.logger.debug("Received device: #{data[:device]}")
-      device = Device.find_or_create_by!(custom_id: data[:device][:custom_id]) do |dev|
-        dev.manufacturer_name = data[:device][:manufacturer_name]
-        dev.description = data[:device][:description]
-        dev.friendly_name = data[:device][:friendly_name]
-        dev.model = data[:device][:model]
-        dev.serial_number = data[:device][:serial_number]
-        dev.firmware_version = data[:device][:firmware_version]
-        dev.software_version = data[:device][:software_version]
-        dev.categories = data[:device][:categories]
+      Rails.logger.debug("Received device: #{data[:device_attributes]}")
+      device = Device.find_or_create_by!(custom_id: data[:device_attributes][:custom_id]) do |dev|
+        dev.manufacturer_name = data[:device_attributes][:manufacturer_name]
+        dev.description = data[:device_attributes][:description]
+        dev.friendly_name = data[:device_attributes][:friendly_name]
+        dev.model = data[:device_attributes][:model]
+        dev.serial_number = data[:device_attributes][:serial_number]
+        dev.firmware_version = data[:device_attributes][:firmware_version]
+        dev.software_version = data[:device_attributes][:software_version]
+        dev.categories = data[:device_attributes][:categories]
       end
       Rails.logger.debug("Created/Found device: #{device.inspect}")
   
       # Sensor
-      Rails.logger.debug("Received sensor: #{data[:sensor]}")
-      sensor = Sensor.find_or_create_by!(serial_number: data[:sensor][:serial_number]) do |sen|
-        sen.manufacturer_name = data[:sensor][:manufacturer_name]
+      Rails.logger.debug("Received sensor: #{data[:sensor_attributes]}")
+      sensor = Sensor.find_or_create_by!(serial_number: data[:sensor_attributes][:serial_number]) do |sen|
+        sen.manufacturer_name = data[:sensor_attributes][:manufacturer_name]
         sen.device_id = device.id
-        sen.serial_number = data[:sensor][:serial_number]
-        sen.category = data[:sensor][:category]
+        sen.serial_number = data[:sensor_attributes][:serial_number]
+        sen.category = data[:sensor_attributes][:category]
       end
       Rails.logger.debug("Created/Found sensor: #{sensor.inspect}")
   
       # TimeOfSample
-      time_of_sample = TimeOfSample.find_or_create_by!(date: data[:time_of_sample][:date])
+      time_of_sample = TimeOfSample.find_or_create_by!(date: data[:time_of_sample_attributes][:date])
       Rails.logger.debug("Created/Found time_of_sample: #{time_of_sample.inspect}")
   
       # Create DataEntry with all related records
