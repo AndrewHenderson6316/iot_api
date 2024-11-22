76954007d7c647d7382fe9c688fdaeeb621552e8
EtienneBoutet8561234 <113942723+EtienneBoutet8561234@users.noreply.github.com>
2024-11-22 17:32:08 -0500

    fix to sensor branch

--- app/controllers/api/v1/iot_data_controller.rb
+++ app/controllers/api/v1/iot_data_controller.rb
@@ -7,11 +7,18 @@ class Api::V1::IotDataController < ApplicationController
 
     ActiveRecord::Base.transaction do
       # Find or create the related records
+      Rails.logger.debug("Starting transaction...")
+    
+      # DataType
+      Rails.logger.debug("Received data_type: #{data[:data_type]}")
       data_type = DataType.find_or_create_by!(typeName: data[:data_type][:typeName]) do |dt|
         dt.scale = data[:data_type][:scale]
         dt.description = data[:data_type][:description]
       end
-
+      Rails.logger.debug("Created/Found data_type: #{data_type.inspect}")
+    
+      # Device
+      Rails.logger.debug("Received device: #{data[:device]}")
       device = Device.find_or_create_by!(custom_id: data[:device][:custom_id]) do |dev|
         dev.manufacturer_name = data[:device][:manufacturer_name]
         dev.description = data[:device][:description]
@@ -22,28 +29,38 @@ class Api::V1::IotDataController < ApplicationController
         dev.software_version = data[:device][:software_version]
         dev.categories = data[:device][:categories]
       end
-      sensor = Sensor.find_or_create_by!(sensor_id: data[:sensor][:sensor_id]) do |dev|
-        dev.manufacturer_name = data[:sensor][:manufacturer_name]
-        dev.device_id = device.id
-        dev.serial_number = data[:device][:serial_number]
-        dev.category = data[:device][:category]
+      Rails.logger.debug("Created/Found device: #{device.inspect}")
+    
+      # Sensor
+      Rails.logger.debug("Received sensor: #{data[:sensor]}")
+      sensor = Sensor.find_or_create_by!(serial_number: data[:sensor][:serial_number]) do |sen|
+        sen.manufacturer_name = data[:sensor][:manufacturer_name]
+        sen.device_id = device.id
+        sen.serial_number = data[:sensor][:serial_number]
+        sen.category = data[:sensor][:category]
       end
+      Rails.logger.debug("Created/Found sensor: #{sensor.inspect}")
+    
+      Rails.logger.debug("Transaction completed successfully.")
+    end
 
       time_of_sample = TimeOfSample.find_or_create_by!(date: data[:time_of_sample][:date])
 
-      # Create the DataEntry record
-      DataEntry.create!(
+     # Create DataEntry with all related records
+    @data_entry = DataEntry.create!(
         value: data[:value],
         data_type_id: data_type.id,
         device_id: device.id,
         time_of_sample_id: time_of_sample.id,
         sensor_id: sensor.id
       )
-    end
 
-    render json: { status: 'success', message: 'Data received and saved' }, status: :created
-  rescue StandardError => e
-    Rails.logger.error "Error saving IoT data: #{e.message}"
-    render json: { status: 'error', message: e.message }, status: :unprocessable_entity
+
+    if @data_entry.save
+      render json: @data_entry, status: :created
+    else
+      Rails.logger.error("Validation Errors: #{@data_entry.errors.full_messages}")
+      render json: { errors: @data_entry.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 end
--- app/controllers/data_entries_controller.rb
+++ app/controllers/data_entries_controller.rb
@@ -14,15 +14,61 @@ class DataEntriesController < ApplicationController
 
   # POST /groups
   def create
-    @data_entry = DataEntry.new(data_entry_params)
-    @data_type = @data_entry.build_data_type
-    @time_of_sample = @data_entry.build_time_of_sample
-    @device = @data_entry.build_device
-    @device.save
-    @sensor = @data_entry.build_sensor
-    @sensor.device_id = @device.id
-
-
+    # Permit nested parameters for data creation
+    data = params.require(:data_entry).permit(:value, data_type: {}, device: {}, time_of_sample: {}, sensor: {})
+  
+    ActiveRecord::Base.transaction do
+      # Logging the start of the transaction
+      Rails.logger.debug("Starting transaction...")
+  
+      # DataType
+      Rails.logger.debug("Received data_type: #{data[:data_type]}")
+      data_type = DataType.find_or_create_by!(typeName: data[:data_type][:typeName]) do |dt|
+        dt.scale = data[:data_type][:scale]
+        dt.description = data[:data_type][:description]
+      end
+      Rails.logger.debug("Created/Found data_type: #{data_type.inspect}")
+  
+      # Device
+      Rails.logger.debug("Received device: #{data[:device]}")
+      device = Device.find_or_create_by!(custom_id: data[:device][:custom_id]) do |dev|
+        dev.manufacturer_name = data[:device][:manufacturer_name]
+        dev.description = data[:device][:description]
+        dev.friendly_name = data[:device][:friendly_name]
+        dev.model = data[:device][:model]
+        dev.serial_number = data[:device][:serial_number]
+        dev.firmware_version = data[:device][:firmware_version]
+        dev.software_version = data[:device][:software_version]
+        dev.categories = data[:device][:categories]
+      end
+      Rails.logger.debug("Created/Found device: #{device.inspect}")
+  
+      # Sensor
+      Rails.logger.debug("Received sensor: #{data[:sensor]}")
+      sensor = Sensor.find_or_create_by!(serial_number: data[:sensor][:serial_number]) do |sen|
+        sen.manufacturer_name = data[:sensor][:manufacturer_name]
+        sen.device_id = device.id
+        sen.serial_number = data[:sensor][:serial_number]
+        sen.category = data[:sensor][:category]
+      end
+      Rails.logger.debug("Created/Found sensor: #{sensor.inspect}")
+  
+      # TimeOfSample
+      time_of_sample = TimeOfSample.find_or_create_by!(date: data[:time_of_sample][:date])
+      Rails.logger.debug("Created/Found time_of_sample: #{time_of_sample.inspect}")
+  
+      # Create DataEntry with all related records
+      @data_entry = DataEntry.create!(
+        value: data[:value],
+        data_type_id: data_type.id,
+        device_id: device.id,
+        time_of_sample_id: time_of_sample.id,
+        sensor_id: sensor.id
+      )
+      Rails.logger.debug("Created data_entry: #{@data_entry.inspect}")
+    end
+  
+    # Render the response based on save status
     if @data_entry.save
       render json: @data_entry, status: :created
     else
@@ -30,7 +76,6 @@ class DataEntriesController < ApplicationController
       render json: { errors: @data_entry.errors.full_messages }, status: :unprocessable_entity
     end
   end
-  
 
   # PATCH/PUT 
   def update
@@ -59,6 +104,7 @@ class DataEntriesController < ApplicationController
         :data_type_id,
         :device_id,
         :time_of_sample_id,
+        :sensor_id,
         time_of_sample_attributes:[
           :date
         ],
@@ -83,7 +129,7 @@ class DataEntriesController < ApplicationController
           :category,
           :created_at,
           :updated_at,
-          :device_id
+          :device_id,
         ]
       )
     end
